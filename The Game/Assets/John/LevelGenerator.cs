50f8421439815bb96b68ee11f2d91be6cbd5c988
MilkBagBoii <johnleonardo207@gmail.com>
2025-07-07 14:30:51 -0400

    Generation Rework WIP

--- The Game/Assets/John/LevelGenerator.cs
+++ The Game/Assets/John/LevelGenerator.cs
@@ -1,20 +1,26 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject[] levelPrefabs;
-    public int numberOfsectors = 4; // Total number of levels to generate
+    public GameObject startRoom;
+    public GameObject[] rooms;
+
+    Transform[] exitPos;
+
+    Transform curExitPos;
+
+
+
     private void Start()
     {
-        for (int i = 0; i < numberOfsectors / 2; i++)
-        {
-            for (int j = 0; j < numberOfsectors / 2; j++)
-            {
-                GameObject level = Instantiate(levelPrefabs[Random.Range(0, levelPrefabs.Length)]);
-                level.transform.position = new Vector3(i * 10, 0, j * 10); // Adjust position for each level
-                level.name = "Level " + (i + 1);
-            }
+       
+    }
 
-        }
+    public void GenerateLevel()
+    {
+
     }
 }
