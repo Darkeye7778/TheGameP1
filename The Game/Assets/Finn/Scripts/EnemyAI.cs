9af8ad68fb8e10b47fa5417438ecfb16225a5502
Finnagain <finnagainlarose@gmail.com>
2025-07-30 00:13:38 -0400

    Added Suicide Bombers

--- The Game/Assets/Finn/Scripts/EnemyAI.cs
+++ The Game/Assets/Finn/Scripts/EnemyAI.cs
@@ -39,6 +39,16 @@ public class enemyAI : MonoBehaviour, IDamagable
     float stoppingDistOrig;
 
     bool playerInTrigger;
+    public bool isBomber;
+
+    enum BomberState { Idle, Locking, Charging, Pausing, Reacquire }
+    BomberState bomberState = BomberState.Idle;
+
+    Vector3 chargeTarget;
+    [SerializeField] float chargeSpeed;
+    [SerializeField] float pauseTime;
+    [SerializeField] float explosionRange;
+    [SerializeField] float explosionDamage;
 
     Vector3 playerDir;
     Vector3 startingPos;
@@ -46,29 +56,29 @@ public class enemyAI : MonoBehaviour, IDamagable
     // Start is called once before the first Update after the MonoBehavior is created 
     void Start()
     {
+        Debug.Log("Start() called on " + gameObject.name);
+        Debug.Log("Bullet is: " + bullet);
+
         colorOrg = model.material.color;
         gameManager.instance.updateTerroristCount(1);
         startingPos = transform.position;
         stoppingDistOrig = agent.stoppingDistance;
+
+        if (bullet == null)
+            isBomber = true;
     }
     // Update is called once per frame 
     void Update()
     {
         _doorCooldown += Time.deltaTime;
-        
-        anim.SetFloat("Speed", agent.velocity.magnitude);
-        if (agent.remainingDistance < 0.01f)
-        {
-            roamTimer += Time.deltaTime;
-        }
 
-        if (playerInTrigger && !canSeePlayer())
+        if (isBomber)
         {
-            roamCheck();
+            BomberUpdate();
         }
-        else if (!playerInTrigger)
+        else
         {
-            roamCheck();
+            NormalUpdate();
         }
 
         if (agent.velocity.magnitude > 0.1f && !playingStep)
@@ -79,6 +89,73 @@ public class enemyAI : MonoBehaviour, IDamagable
         if(_doorCooldown > 1)
             CheckDoor();
     }
+
+    void NormalUpdate()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+
+        if (agent.remainingDistance < 0.01f)
+            roamTimer += Time.deltaTime;
+
+        if (playerInTrigger && !canSeePlayer())
+            roamCheck();
+        else if (!playerInTrigger)
+            roamCheck();
+    }
+
+    void BomberUpdate()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+
+        switch (bomberState)
+        {
+            case BomberState.Idle:
+                if (BomberCanSeePlayer())
+                {
+                    agent.ResetPath();
+
+                    chargeTarget = gameManager.instance.player.transform.position;
+                    chargeTarget.y = transform.position.y;
+                    agent.speed = chargeSpeed;
+                    agent.stoppingDistance = 0;
+                    agent.SetDestination(chargeTarget);
+                    bomberState = BomberState.Charging;
+                }
+                else
+                {
+                    roamTimer += Time.deltaTime;
+                    roamCheck();
+                }
+                    break;
+
+            case BomberState.Charging:
+                if (Vector3.Distance(transform.position, chargeTarget) < 0.5f)
+                {
+                    bomberState = BomberState.Pausing;
+                    StartCoroutine(PauseAndReacquire());
+                }
+                break;
+
+            case BomberState.Pausing:
+                // wait handled in coroutine
+                break;
+
+            case BomberState.Reacquire:
+                if (BomberCanSeePlayer())
+                {
+                    chargeTarget = gameManager.instance.player.transform.position;
+                    chargeTarget.y = transform.position.y;
+                    agent.SetDestination(chargeTarget);
+                    bomberState = BomberState.Charging;
+                }
+                else
+                {
+                    bomberState = BomberState.Idle;
+                }
+                break;
+        }
+    }
+
     void roamCheck()
     {
         if (roamTimer >= roamPauseTime && agent.remainingDistance < 0.01f)
@@ -129,6 +206,22 @@ public class enemyAI : MonoBehaviour, IDamagable
         agent.stoppingDistance = 0;
         return false;
     }
+    bool BomberCanSeePlayer()
+    {
+        Vector3 bomberDir = gameManager.instance.player.transform.position - headPos.position;
+        float bomberAngleToPlayer = Vector3.Angle(bomberDir, transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, bomberDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && bomberAngleToPlayer <= fov)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     void CheckDoor()
     {
@@ -155,6 +248,11 @@ public class enemyAI : MonoBehaviour, IDamagable
         if (other.CompareTag("Player"))
         {
             playerInTrigger = true;
+
+            if (isBomber)
+            {
+                Explode();
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -229,6 +327,26 @@ public class enemyAI : MonoBehaviour, IDamagable
         return weights.Length - 1; // fallback in case of rounding
     }
 
+    void Explode()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRange);
+        foreach (var hit in hitColliders)
+        {
+            IDamagable damagable = hit.GetComponent<IDamagable>();
+            if (damagable != null && hit.CompareTag("Player"))
+            {
+                DamageSource dmg = new DamageSource("Bomber", gameObject);
+                damagable.OnTakeDamage(dmg, explosionDamage);
+            }
+        }
+
+        // Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+        gameManager.instance.updateTerroristCount(-1);
+        Destroy(gameObject);
+    }
+
+
     IEnumerator playFootstep()
     {
         if (playingStep)
@@ -239,5 +357,11 @@ public class enemyAI : MonoBehaviour, IDamagable
         audioSource.Play();
         yield return new WaitForSeconds(audioSource.clip.length);
         playingStep = false;
+    }
+
+    IEnumerator PauseAndReacquire()
+    {
+        yield return new WaitForSeconds(pauseTime);
+        bomberState = BomberState.Reacquire;
     }
 }
